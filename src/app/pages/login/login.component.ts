5d9bce08a155d0466fa5675463268c5d8bc042b4
ali-bouali <bouali.pro@gmail.com>
2023-04-02 15:19:20 +0200

    implements login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import {Router} from "@angular/router";
+import {AuthenticationRequest} from "../../services/models/authentication-request";
+import {AuthenticationService} from "../../services/services/authentication.service";
+import {TokenService} from "../../services/token-service/token.service";
 
 @Component({
   selector: 'app-login',
@@ -8,11 +11,34 @@ import {Router} from "@angular/router";
 })
 export class LoginComponent {
 
+  authRequest: AuthenticationRequest = {};
+  errorMsg = '';
+
   constructor(
-    private router: Router
+    private router: Router,
+    private authService: AuthenticationService,
+    private tokenService: TokenService
   ) {}
 
   register() {
     this.router.navigate(['register']);
+  }
+
+  login() {
+    this.errorMsg = '';
+    this.authService.login(
+      {
+        body: this.authRequest
+      }
+    ).subscribe({
+      next: (response) => {
+        this.tokenService.saveResponse(response);
+        this.router.navigate(['dashboard']);
+      },
+      error: (err) => {
+        console.log(err);
+        this.errorMsg = err.error.errorMsg;
+      }
+    });
   }
 }
