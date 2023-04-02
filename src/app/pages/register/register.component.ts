05ec95f39bc48c0ee870d7872154b516156990a1
ali-bouali <bouali.pro@gmail.com>
2023-04-02 13:38:31 +0200

    register page

---
+++ src/app/pages/register/register.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent {
+
+  constructor(
+    private router: Router
+  ) {}
+
+  login() {
+    this.router.navigate(['login']);
+  }
+}
