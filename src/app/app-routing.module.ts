27c21ba3c45525ce4db45a4583087a3db088f1ff
ali-bouali <bouali.pro@gmail.com>
2023-04-02 13:40:17 +0200

    bootstrap + font-awesome config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,54 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./pages/login/login.component";
+import {RegisterComponent} from "./pages/register/register.component";
+import {MainComponent} from "./pages/main/main.component";
+import {DashboardComponent} from "./pages/dashboard/dashboard.component";
+import {TransactionsComponent} from "./pages/transactions/transactions.component";
+import {ContactsComponent} from "./pages/contacts/contacts.component";
+import {ManageContactsComponent} from "./pages/manage-contacts/manage-contacts.component";
+import {NewTransactionComponent} from "./pages/new-transaction/new-transaction.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: 'transactions',
+        component: TransactionsComponent
+      },
+      {
+        path: 'contacts',
+        component: ContactsComponent
+      },
+      {
+        path: 'contacts/manage',
+        component: ManageContactsComponent
+      },
+      {
+        path: 'contacts/manage/:contactId',
+        component: ManageContactsComponent
+      },
+      {
+        path: 'transactions/new',
+        component: NewTransactionComponent
+      },
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { MainComponent } from './pages/main/main.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { MenuItemComponent } from './components/menu-item/menu-item.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { TransactionsComponent } from './pages/transactions/transactions.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { ManageContactsComponent } from './pages/manage-contacts/manage-contacts.component';
+import { NewTransactionComponent } from './pages/new-transaction/new-transaction.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    MainComponent,
+    MenuComponent,
+    MenuItemComponent,
+    DashboardComponent,
+    TransactionsComponent,
+    ContactsComponent,
+    ManageContactsComponent,
+    NewTransactionComponent
   ],
   imports: [
     BrowserModule,
