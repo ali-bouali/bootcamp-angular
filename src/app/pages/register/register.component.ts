939e3d6fc9e403d12658a6eda176bf3417257a99
ali-bouali <bouali.pro@gmail.com>
2023-04-02 15:19:27 +0200

    implements register

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import {Router} from "@angular/router";
+import {RegisterRequest} from "../../services/models/register-request";
+import {AuthenticationService} from "../../services/services/authentication.service";
+import {TokenService} from "../../services/token-service/token.service";
 
 @Component({
   selector: 'app-register',
@@ -8,11 +11,30 @@ import {Router} from "@angular/router";
 })
 export class RegisterComponent {
 
+  registerRequest: RegisterRequest = {email: '', firstname: '', lastname: '', password: ''};
+  errorMsgs: string[] = [];
   constructor(
-    private router: Router
+    private router: Router,
+    private authService: AuthenticationService,
+    private tokenService: TokenService
   ) {}
 
   login() {
     this.router.navigate(['login']);
+  }
+
+  register() {
+    this.errorMsgs = [];
+    this.authService.register({
+      body: this.registerRequest
+    }).subscribe({
+      next: (res) => {
+        this.tokenService.saveResponse(res);
+        this.router.navigate(['register-success'])
+      },
+      error: (err) => {
+        this.errorMsgs = err.error.validationErrors;
+      }
+    });
   }
 }
