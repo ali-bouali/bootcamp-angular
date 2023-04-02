3c26d6f39a4d13ec98952ea05536be6b49591f69
ali-bouali <bouali.pro@gmail.com>
2023-04-02 13:39:48 +0200

    new transactions

---
+++ src/app/pages/new-transaction/new-transaction.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NewTransactionComponent } from './new-transaction.component';
+
+describe('NewTransactionComponent', () => {
+  let component: NewTransactionComponent;
+  let fixture: ComponentFixture<NewTransactionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NewTransactionComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(NewTransactionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/new-transaction/new-transaction.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-transaction',
+  templateUrl: './new-transaction.component.html',
+  styleUrls: ['./new-transaction.component.scss']
+})
+export class NewTransactionComponent {
+
+}
