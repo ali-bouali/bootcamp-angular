72cc1c794d7ead7189b6ee69c092b22d40a47f25
ali-bouali <bouali.pro@gmail.com>
2023-04-15 13:47:44 +0200

    add admin main comp

---
+++ src/app/admin/admin-main/admin-main.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AdminMainComponent } from './admin-main.component';
+
+describe('AdminMainComponent', () => {
+  let component: AdminMainComponent;
+  let fixture: ComponentFixture<AdminMainComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AdminMainComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AdminMainComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/admin/admin-main/admin-main.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-main',
+  templateUrl: './admin-main.component.html',
+  styleUrls: ['./admin-main.component.scss']
+})
+export class AdminMainComponent {
+
+}
